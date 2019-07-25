2195d6f5ea6428cac5472d4821a856c387d86a54
fuckcpp <m18588290231@163.com>
2019-07-25 19:43:13 +0800

    UE4 firstperson模板 创建游戏项目

---
+++ Source/Game/GameHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "GameHUD.generated.h"
+
+UCLASS()
+class AGameHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AGameHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
